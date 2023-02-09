2ea658e8ecc00d1c7a9730eedbd4a05305ca195a
Cagan <cagansvncn@gmail.com>
2023-02-09 15:47:29 -0800

    Commit

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-const User = require('./models/user')
-const Business = require('./models/business')
-const Product = require('./models/Enums/Product')
+const express = require('express');
+const bodyParser = require('body-parser');
+const businessRouter = require('./routes/business');
+const indexRouter = require('./routes/index');
+
+require('./mongo-connection')
+
+const app = express();
+//Bodyparser middleware oldugu icin boyle kullaniyoruz
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+
+app.use('/passengers', businessRouter);
+app.use('/', indexRouter);
 
 
-const John = User.create({
-    name: 'John',
-    email: 'jondoe@gmail.com',
-    phone: '123456789',
-    location: '1234 Main St'
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 })
 
-const Farm = John.createBusiness('Johns Farm', 'We sell fresh produce', 'cagan@gmail.com', '123456789')
-Farm.businessDetails.category = 'Farm'
-Farm.businessDetails.practices = ['RegenerativeFarming', 'Biodynamic',]
-Farm.businessDetails.product = ['Wine', 'FermentedFoods', 'Beer',]
-const products = Product.categories
 
 
-console.log(products)
-
-John.favThisBusiness(Farm)
 
 
-Farm.businessDetails.product = products
 
-console.log(John)
-console.log(Farm)
+
+
+
+
+
+
+
+
+
+
+
+
+// const John = User.create({
+//     name: 'John',
+//     email: 'jondoe@gmail.com',
+//     phone: '123456789',
+//     location: '1234 Main St'
+// })
+
+// const Farm = John.createBusiness('Johns Farm', 'We sell fresh produce', 'cagan@gmail.com', '123456789')
+// Farm.businessDetails.category = 'Farm'
+// Farm.businessDetails.practices = ['RegenerativeFarming', 'Biodynamic',]
+// Farm.businessDetails.product = ['Wine', 'FermentedFoods', 'Beer',]
+// const products = Product.categories
+
+
+// console.log(products)
+
+// John.favThisBusiness(Farm)
+
+
+// Farm.businessDetails.product = products
+
+// console.log(John)
+// console.log(Farm)
+
+
 
---
+++ mongo-connection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", true);
+
+mongoose.createConnection('mongodb://127.0.0.1/shire-app', { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+console.log("connecting");
+
+db.once('open', () => {
+    console.log("we're connected!");
+});
+
+try {
+    mongoose.connect('mongodb://127.0.0.1/shire-app');
+} catch (err) {
+    console.error(err);
+}
